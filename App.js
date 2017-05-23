01a85e6fabb03d19bbaedc572d05d97f83221149
mizchi <miz404@gmail.com>
2017-05-23 22:32:56 +0900

    Add eslint

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/* @flow */
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Changes you make will automatically reload.</Text>
+      <Text>Shake your phone to open the developer menu.</Text>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,6 +17,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
